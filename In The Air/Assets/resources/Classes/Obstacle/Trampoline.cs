d3d2f9025c7018fc86c06a46f5613eff28acaf6a
Ryan Vacek <rvacek@ucsc.edu>
2016-01-30 12:33:34 -0800

    -Classes for obstacle, trampoline, and person added -Temporary bounce for trampoline added -Prefabs for trampoline and person added

---
+++ In The Air/Assets/resources/Classes/Obstacle/Trampoline.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trampoline : Obstacle {
+	[SerializeField] protected float bounceCoefficient = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		Vector2 newVel = coll.rigidbody.velocity;
+		newVel.Normalize();
+		newVel = -10 * Vector2.Dot(newVel, (Vector2) transform.up) * (Vector2) transform.up + newVel;
+		this.durability -= 1;
+		coll.rigidbody.velocity = newVel;
+	}
+}
