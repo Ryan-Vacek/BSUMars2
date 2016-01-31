43ec6916a386f5f8e1b542572283a45c21958bec
Eric Anderson <hirorir@gmail.com>
2016-01-31 01:28:14 -0800

    adding spawnericon (drag and drop script) prefab

--- In The Air/Assets/resources/Classes/Person.cs
+++ In The Air/Assets/resources/Classes/Person.cs
@@ -17,7 +17,7 @@ public class Person : MonoBehaviour {
 	
 	}
 
-	void OnCollisionStay2D(Collision2D collision) {
+	void OnCollisionEnter2D(Collision2D collision) {
 		Instantiate (blood, transform.position, transform.rotation);
 	}
 
---
+++ In The Air/Assets/resources/Classes/TrampolineIcon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrampolineIcon : MonoBehaviour {
+
+	[SerializeField] GameObject spawnObject;
+	bool mouseDown = false;
+	Vector3 startingPos;
+
+	// Use this for initialization
+	void Start () {
+		startingPos = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mouseDown) {
+			Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			pos.z = gameObject.transform.position.z;
+			gameObject.transform.position = pos;
+		} else {
+			gameObject.transform.position = startingPos;
+		}
+	}
+
+	void OnMouseDown() {
+		gameObject.GetComponent<SpriteRenderer> ().color = Color.green;
+		mouseDown = true;
+	}
+
+	void OnMouseUp() {
+		// Do some magic check here to see if this is a valid spot to drop a trampoline
+		Instantiate(spawnObject, gameObject.transform.position, gameObject.transform.rotation);
+		gameObject.GetComponent<SpriteRenderer> ().color = Color.white;
+		mouseDown = false;
+	}
+}
